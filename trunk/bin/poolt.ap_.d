61de1f65bbcedb196e7f729d864e4094d3a19f94
paskuda@gmail.com <paskuda@gmail.com@7d41fa91-266a-23cc-1729-df17da6fa176>
2011-12-13 10:30:15 +0000

    Initial import.

---
+++ trunk/bin/poolt.ap_.d
@@ -0,0 +1,14 @@
+C:\Users\leon\Projects\Poolt\bin\poolt.ap_ \
+ : C:\Users\leon\Projects\Poolt\res\drawable\poolt.png \
+C:\Users\leon\Projects\Poolt\res\drawable-hdpi\poolt.png \
+C:\Users\leon\Projects\Poolt\res\drawable-ldpi\poolt.png \
+C:\Users\leon\Projects\Poolt\res\drawable-mdpi\poolt.png \
+C:\Users\leon\Projects\Poolt\res\drawable-xhdpi\poolt.png \
+C:\Users\leon\Projects\Poolt\res\layout\main.xml \
+C:\Users\leon\Projects\Poolt\res\values\strings.xml \
+C:\Users\leon\Projects\Poolt\bin\res\drawable\poolt.png \
+C:\Users\leon\Projects\Poolt\bin\res\drawable-hdpi\poolt.png \
+C:\Users\leon\Projects\Poolt\bin\res\drawable-ldpi\poolt.png \
+C:\Users\leon\Projects\Poolt\bin\res\drawable-mdpi\poolt.png \
+C:\Users\leon\Projects\Poolt\bin\res\drawable-xhdpi\poolt.png \
+C:\Users\leon\Projects\Poolt\AndroidManifest.xml \
